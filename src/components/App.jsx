8522dffc3805469228694eea54dadfada0e6b27b
Katherine <katherinetorres@uninorte.edu.co>
2021-11-27 22:33:18 -0500

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/style.scss';
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+	return (
+        <div>
+            <h1>Master Barber Shop</h1>
+            <button className="btn btn-primary">Bootstrap</button>
+
+        </div>
+	);
+}
+
+export default App;
